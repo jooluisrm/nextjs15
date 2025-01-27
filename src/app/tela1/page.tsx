f7035e1596b5885211eb82c90864708f23d9862c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-01-27 08:50:54 -0300

    Formas de mudar de rota

---
+++ src/app/tela1/page.tsx
@@ -0,0 +1,14 @@
+import { Tela2Button } from "@/components/tela2Button";
+
+const Tela1 = () => {
+    return (
+        <div>
+            <h1>Pagina Tela 1</h1>
+            
+            <Tela2Button />
+        </div>
+    );
+}
+
+
+export default Tela1;
---
+++ src/app/tela2/page.tsx
@@ -0,0 +1,15 @@
+import { redirect } from "next/navigation";
+
+const Tela2 = () => {
+
+    redirect("/")
+    
+    return (
+        <div>
+            Pagina TEla2
+        </div>
+    );
+}
+
+
+export default Tela2;
